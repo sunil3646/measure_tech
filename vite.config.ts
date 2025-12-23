afaf1533d490183e7a7b41d9a8be746701a0db58
sunil3646 <153943298+sunil3646@users.noreply.github.com.>
2025-12-23 23:44:13 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
-
+import componentTagger from "vite-plugin-component-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
