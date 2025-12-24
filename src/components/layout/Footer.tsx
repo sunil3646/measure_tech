39c49690cbd6382df5aea6c751f14772291e8907
sunil3646 <153943298+sunil3646@users.noreply.github.com.>
2025-12-24 11:34:40 +0530

    updated

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import logo from '../../assets/logo.png';
+import logo from '@/assets/favicon.ico';
 import {
   Zap,
   Mail,
